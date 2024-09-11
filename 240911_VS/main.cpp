6d0eaa6001142283df2c48cdd67c99e941754c6e
xinyang20--XM <15850645317@163.com>
2024-09-11 22:04:44 +0800

    提交240911部分代码

---
+++ 240911_VS/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "LinkList.h"
+
+using namespace std;
+
+int main() {
+	int a[4] = { 4,1,2,3 }, b[4] = { 5,1,3,6 };
+	LinkList<int> A(a, 4), B(b, 4);
+	cout << "A:";
+	A.PrintList();
+	cout << "B:";
+	B.PrintList();
+
+	LinkList<int> C(A.Union(B));
+	cout << "并集C:";
+	C.PrintList();
+
+	LinkList<int> D(A.Intersection(B));
+	cout << "交集D:";
+	D.PrintList();
+
+	LinkList<int> E(A.Difference(B));
+	cout << "差集E:";
+	E.PrintList();
+
+	return 0;
+}
